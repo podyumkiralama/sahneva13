83ef69d20ce860a5c86ea231cef54a07ed730529
Murat <kasimrize@gmail.com>
2026-03-17 02:28:09 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -104,7 +104,7 @@ const securityHeaders = (() => {
     {
       key: "Permissions-Policy",
       value:
-        "camera=(), microphone=(), geolocation=(), browsing-topics=(), payment=(), fullscreen=(self \"https://www.youtube.com\" \"https://www.youtube-nocookie.com\")",
+        "camera=(), microphone=(), geolocation=(), browsing-topics=(), payment=(), fullscreen=()",
     },
     {
       key: "Strict-Transport-Security",
@@ -147,7 +147,6 @@ const nextConfig = {
     deviceSizes: [320, 420, 640, 750, 828, 1080, 1200, 1440, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/avif", "image/webp"],
-    qualities: [60, 75],
     minimumCacheTTL: ONE_MONTH_IN_SECONDS,
     remotePatterns: [],
     dangerouslyAllowSVG: false,
