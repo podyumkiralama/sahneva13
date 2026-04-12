034b35bf360abb12172c803d1c262620e5489a8c
Murat <kasimrize@gmail.com>
2026-04-12 12:40:39 +0300

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -2,7 +2,7 @@
 
 export default function robots() {
   const host = "https://www.sahneva.com";
-  const disallow = ["/api/", "/private/", "/search"];
+  const disallow = ["/api/", "/private/"];
 
   return {
     rules: [
