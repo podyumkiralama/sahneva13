afa97302fdd8ab72aec5307efb1fa0d016ffd61d
Murat <kasimrize@gmail.com>
2026-06-18 00:14:19 +0300

    Add final trust copy and international FAQ items

--- app/ru/event-production-company-turkey/page.js
+++ app/ru/event-production-company-turkey/page.js
@@ -1,4 +1,5 @@
 import EventProductionPartnerPage from "@/components/international/EventProductionPartnerPage";
+import PubgFinalVideoReference from "@/components/international/PubgFinalVideoReference";
 import { buildInternationalEventMetadata } from "@/lib/internationalEventProduction";
 
 export const revalidate = 1800;
@@ -6,5 +7,10 @@ export const revalidate = 1800;
 export const metadata = buildInternationalEventMetadata("ru");
 
 export default function RussianEventProductionCompanyTurkeyPage() {
-  return <EventProductionPartnerPage locale="ru" />;
+  return (
+    <>
+      <EventProductionPartnerPage locale="ru" />
+      <PubgFinalVideoReference locale="ru" />
+    </>
+  );
 }
