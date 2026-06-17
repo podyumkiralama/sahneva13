5d836a70768ec5f20173de502adcfea78f134685
Murat <kasimrize@gmail.com>
2026-06-18 00:04:39 +0300

    Add final trust copy and international FAQ items

--- app/en/event-production-company-turkey/page.js
+++ app/en/event-production-company-turkey/page.js
@@ -1,4 +1,5 @@
 import EventProductionPartnerPage from "@/components/international/EventProductionPartnerPage";
+import InternationalTrustPolish from "@/components/international/InternationalTrustPolish";
 import { buildInternationalEventMetadata } from "@/lib/internationalEventProduction";
 
 export const revalidate = 1800;
@@ -6,5 +7,10 @@ export const revalidate = 1800;
 export const metadata = buildInternationalEventMetadata("en");
 
 export default function EventProductionCompanyTurkeyPage() {
-  return <EventProductionPartnerPage locale="en" />;
+  return (
+    <>
+      <EventProductionPartnerPage locale="en" />
+      <InternationalTrustPolish locale="en" />
+    </>
+  );
 }
