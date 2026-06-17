1f6f222ff72ba9e7336ab0d38adb1be6a41ad80c
Murat <kasimrize@gmail.com>
2026-06-18 01:41:35 +0300

    Remove duplicate hero company profile link

--- app/(tr)/turkiyede-etkinlik-cozum-ortagi/page.jsx
+++ app/(tr)/turkiyede-etkinlik-cozum-ortagi/page.jsx
@@ -5,6 +5,25 @@ export const revalidate = 1800;
 
 export const metadata = buildInternationalEventMetadata("tr");
 
+function HideDuplicateHeroPdfCard() {
+  return (
+    <style
+      dangerouslySetInnerHTML={{
+        __html: `
+          main > section:first-child > div.relative > div:last-child > a[href="/files/sahneva-company-profile.pdf"] {
+            display: none;
+          }
+        `,
+      }}
+    />
+  );
+}
+
 export default function TurkiyedeEtkinlikCozumOrtagiPage() {
-  return <EventProductionPartnerPage locale="tr" />;
+  return (
+    <>
+      <HideDuplicateHeroPdfCard />
+      <EventProductionPartnerPage locale="tr" />
+    </>
+  );
 }
