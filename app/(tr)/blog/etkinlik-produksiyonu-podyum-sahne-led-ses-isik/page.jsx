36f56653aef95aaa7ba01cdee9ee42eb97d7577d
Murat <kasimrize@gmail.com>
2026-08-05 12:42:40 +0300

    Update page.jsx

--- app/(tr)/blog/etkinlik-produksiyonu-podyum-sahne-led-ses-isik/page.jsx
+++ app/(tr)/blog/etkinlik-produksiyonu-podyum-sahne-led-ses-isik/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+import Link from "next/link"; 
 
 import BlogLayout from "@/components/blog/BlogLayout";
 import BlogRelatedLinks from "@/components/blog/BlogRelatedLinks";
