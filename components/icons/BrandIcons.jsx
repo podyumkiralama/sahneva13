683d3ccea862ba887474b435396d135bafdb4dd3
Murat <kasimrize@gmail.com>
2026-08-19 18:31:11 +0300

    Add Facebook brand icon

--- components/icons/BrandIcons.jsx
+++ components/icons/BrandIcons.jsx
@@ -47,3 +47,9 @@ export const Youtube = (props) => (
     <path d="m10 15 5-3-5-3z" />
   </BaseIcon>
 );
+
+export const Facebook = (props) => (
+  <BaseIcon {...props}>
+    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+  </BaseIcon>
+);
